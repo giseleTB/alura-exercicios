ce432b538a0f52cc95c68f243b71f05458a42e3b
Gisele Tonello Bringhenti <giseletbrin@gmail.com>
2023-01-24 23:26:16 -0300

    feat: function toca som

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,1 +1,26 @@
-document.querySelector('.tecla_pom');
+//Pom
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string 
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
